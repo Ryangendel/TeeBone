26f359d6c275c553b645a63253948478a200fe80
tomssmith17 <thsmith@clarku.edu>
2020-01-12 17:18:31 -0700

    learn yo abcs fool

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ var main ="this is the main file";
 var hello = function(){
     console.log("I did a lot of work on this main page")
 };
+
+console.log("a")
+console.log("b")
+console.log("c")
+console.log("d")
+console.log("e")
+console.log("f")
+console.log("g")
+console.log("h")
+console.log("i")
+console.log("j")
