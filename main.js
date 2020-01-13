b36fdf4182749b67496007efef05f9adf7ca2999
rgendel <ryangendel@u.northwestern.edu>
2020-01-12 17:18:57 -0700

    numbers

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ var main ="this is the main file";
 var hello = function(){
     console.log("I did a lot of work on this main page")
 };
+
+console.log("1")
+console.log("2")
+console.log("3")
+console.log("4")
+console.log("5")
+console.log("6")
+console.log("7")
+console.log("8")
+console.log("9")
+console.log("10")
